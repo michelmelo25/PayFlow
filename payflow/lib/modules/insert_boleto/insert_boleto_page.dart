f772ee69f5a8947e70a75d61c3b1e89706a4dd65
Michel Melo <michelmelosj25@hotmail.com>
2021-06-29 09:39:08 -0300

    boleto title

--- payflow/lib/modules/insert_boleto/insert_boleto_page.dart
+++ payflow/lib/modules/insert_boleto/insert_boleto_page.dart
@@ -115,8 +115,9 @@ class _InsertBoletoPageState extends State<InsertBoletoPage> {
           Navigator.pop(context);
         },
         secondaryLabel: "Cadastrar",
-        secondaryOnPressed: () {
-          controller.cadastrarBoleto();
+        secondaryOnPressed: () async {
+          await controller.cadastrarBoleto();
+          Navigator.pop(context);
         },
         enableSecondaryColor: true,
       ),
