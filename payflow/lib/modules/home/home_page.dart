318ee5f904c6693c032012143d89319fde9584f6
Michel Melo <michelmelosj25@hotmail.com>
2021-06-29 11:31:51 -0300

    voltando a chamar scanner barcode

--- payflow/lib/modules/home/home_page.dart
+++ payflow/lib/modules/home/home_page.dart
@@ -82,8 +82,7 @@ class _HomePageState extends State<HomePage> {
                 )),
             GestureDetector(
               onTap: () {
-                // Navigator.pushNamed(context, "/barcode_scanner");
-                Navigator.pushNamed(context, "/insert_boleto");
+                Navigator.pushNamed(context, "/barcode_scanner");
               },
               child: Container(
                 width: 56,
