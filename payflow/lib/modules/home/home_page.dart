2d26f8e57882be7bba4c7d33b9974ce3fff960a6
Michel Melo <michelmelosj25@hotmail.com>
2021-06-29 10:33:02 -0300

    meus boletos page

--- payflow/lib/modules/home/home_page.dart
+++ payflow/lib/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/modules/meus_boletos/meus_boletos_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/boleto_list/boleto_list_widget.dart';
@@ -14,9 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final controller = HomeControllher();
   final pages = [
-    Container(
-      child: BoletoListWidget(),
-    ),
+    MeusBoletosPage(),
     Container(
       color: Colors.blue,
     )
---
+++ payflow/lib/modules/meus_boletos/meus_boletos_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_widget.dart';
+
+class MeusBoletosPage extends StatefulWidget {
+  const MeusBoletosPage({Key? key}) : super(key: key);
+
+  @override
+  _MeusBoletosPageState createState() => _MeusBoletosPageState();
+}
+
+class _MeusBoletosPageState extends State<MeusBoletosPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 24),
+            child: Row(
+              children: [
+                Text(
+                  "Meus Boletos",
+                  style: TextStyles.titleBoldHeading,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24),
+            child: Divider(
+              thickness: 1,
+              height: 1,
+              color: AppColors.stroke,
+            ),
+          ),
+          BoletoListWidget()
+        ],
+      ),
+    );
+  }
+}
